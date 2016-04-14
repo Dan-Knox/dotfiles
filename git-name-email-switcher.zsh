83ae6bc3154e59e94df90740421350ba14513ec2
Dan Knox <dknox@threedotloft.com>
2016-04-14 11:19:22 -0700

    Combine tmux config files into a single tmux.conf.

--- git-name-email-switcher.zsh
+++ git-name-email-switcher.zsh
@@ -6,11 +6,11 @@ set -o errexit
 
 git(){
     case "$PWD" in
-        ${HOME}/vision_tree/*)
+        ${HOME}/evident/*)
             export GIT_AUTHOR_NAME="Dan Knox"
-            export GIT_AUTHOR_EMAIL="dknox@visiontree.com"
+            export GIT_AUTHOR_EMAIL="dknox@evident.io"
             export GIT_COMMITTER_NAME="Dan Knox"
-            export GIT_COMMITTER_EMAIL="dknox@visiontree.com"
+            export GIT_COMMITTER_EMAIL="dknox@evident.io"
             ;;
         *)
             export GIT_AUTHOR_NAME="Dan Knox"
